7df8aedd87c4e193211e7c9fd7b8d822a4d9f727
Afiye Geçgel <afiyegecgel@gmail.com>
2021-01-27 01:45:48 +0300

    _

--- WinFormAppEFCore/Program.cs
+++ WinFormAppEFCore/Program.cs
@@ -17,6 +17,7 @@ namespace WinFormAppEFCore
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new store());
+
         }
     }
 }
